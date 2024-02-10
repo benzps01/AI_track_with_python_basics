3adb1ba2836e744cf725b604a4f82d8afa6b055d
benzps01 <benzps01@gmail.com>
2024-02-11 00:42:07 +0530

    classes example

---
+++ OOPS/example1.py
@@ -0,0 +1,29 @@
+"""
+    Short example on Class, attributes and methods
+"""
+
+
+class Shirt:
+    def __init__(self, shirt_color, shirt_size, shirt_style, shirt_price):
+        self.color = shirt_color
+        self.size = shirt_size
+        self.style = shirt_style
+        self.price = shirt_price
+
+    def change_price(self, new_price):
+        self.price = new_price
+
+    def discount(self, discount):
+        return self.price * (1 - discount)
+
+
+new_shirt = Shirt("red", "S", "short sleeve", 15)
+print(new_shirt.color)
+print(new_shirt.size)
+print(new_shirt.style)
+print(new_shirt.price)
+
+new_shirt.change_price(10)
+print(new_shirt.price)
+
+print(new_shirt.discount(0.2))
